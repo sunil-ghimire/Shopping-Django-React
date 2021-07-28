1e2d021f11c1b17f0afdb388d0529757a9779544
pox420 <pox420@gmail.com>
2021-07-28 09:27:52 +0545

    first commit

---
+++ api/category/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Category
+from .serializers import CategorySerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by('name')
+    serializer_class = CategorySerializer
---
+++ api/product/urls.py
@@ -0,0 +1,11 @@
+from rest_framework import routers, urlpatterns
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'', views.ProductViewSet)
+
+urlpatterns = [
+    path('',include(router.urls))
+]
